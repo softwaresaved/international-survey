910ad2164ac7d4c028c0c6aaae840f426c7edbb8
Olivier PHILIPPE <olivier.philippe@gmail.com>
2017-03-23 17:53:49 +0000

    Progress on discipline -- Done

--- canarie_2017/CANARIE+--+2017.py
+++ canarie_2017/CANARIE+--+2017.py
@@ -1,8 +1,7 @@
-
 # coding: utf-8
 
 # # CANARIE 2017 -- Analysis of the survey
-# 
+
 # This notebook is the first draft to analyse the results from [CANARIE]()
 
 # ## Preparation and filtering of the dataset
@@ -10,10 +9,23 @@
 # In[1]:
 
 # Load libraries
+
 import pandas as pd
+import matplotlib
+
+# When using Ipython within vim
+matplotlib.use('TkAgg')
+
+# When using within jupyter
+# get_ipython().magic('matplotlib inline')  # Activat that line to use in Jupyter
+
 import matplotlib.pyplot as plt
-get_ipython().magic('matplotlib inline')
-import csv
+
+#  When using this script with ipython and vim
+plt.ion()
+plt.show()
+
+import numpy as np
 
 
 # In[2]:
@@ -29,7 +41,7 @@ len(df.index)
 
 # In[3]:
 
-#df['Date submitted'].unique
+# df['Date submitted'].unique
 df = df[df['Date submitted'].notnull()]
 # Getting the number of row from the reduced dataframe
 len(df.index)
@@ -37,14 +49,14 @@ len(df.index)
 
 # ### Countries
 # A question asked the participants in which country they were currently working and specify when it was not Canada.
-# 
+#
 
 # In[4]:
 
 pd.crosstab(df['In which country do you work?'], columns='Countries')
 
 
-# There is only 4 others. Which are only one from Japan, 2 from USA and one from UK.  
+# There is only 4 others. Which are only one from Japan, 2 from USA and one from UK.
 
 # In[5]:
 
@@ -61,7 +73,7 @@ len(df.index)
 
 
 # ### Writing software as part of the work
-# 
+#
 # One question asked if the participants write software for research as part of their work.
 
 # In[7]:
@@ -79,11 +91,11 @@ len(df.index)
 
 
 # ## Univariate analysis
-# 
-# 
+#
+#
 
 # ### Languages
-# 
+#
 # The survey was in French and in English. The option choose by the participant was collected. It is possible then to see the proportion of participants that answered the survey in French or in English
 
 # In[9]:
@@ -106,43 +118,67 @@ df['What is the highest level of education you have attained?'] = df['What is th
 
 # Reorder the factors to match the education level
 df['What is the highest level of education you have attained?'].cat.reorder_categories(['Some University',
-                                                                                         'College Diploma',
-                                                                                         'Bachelors Degree',
-                                                                                         'Masters Degree',
-                                                                                         'Doctorate'],
+                                                                                        'College Diploma',
+                                                                                        'Bachelors Degree',
+                                                                                        'Masters Degree',
+                                                                                        'Doctorate'],
                                                                                        inplace=True)
 pd.crosstab(df['What is the highest level of education you have attained?'], columns='Education level')
 
 
 # In[12]:
 
-df['What is the highest level of education you have attained?'].value_counts().plot(kind='bar', sort_columns=True)
+df['What is the highest level of education you have attained?'].value_counts().plot(kind='barh', sort_columns=True)
 
 
 # ### Discipline
-# 
-# In which discipline the participants obtained their highest qualification. The answers were from the [NSERC codes](http://www.nserc-crsng.gc.ca/Help-Aide/Codes-ListeDeCodes_Eng.asp). 
+#
+# In which discipline the participants obtained their highest qualification. The answers were from the [NSERC codes](http://www.nserc-crsng.gc.ca/Help-Aide/Codes-ListeDeCodes_Eng.asp).
 # However, it is the option 'Other', followed by a freetext option, which is the most chosen.
-# Therefore, before plotting it, we need to clean and merge these answers with the NESRC ones. 
+# Therefore, before plotting it, we need to clean and merge these answers with the NESRC ones.
 
 # #### Discipline -- Other
-# 
-# 
+#
+#
 
 # In[19]:
 
-df['In which discipline is your highest academic qualification? [Other]'] = df['In which discipline is your highest academic qualification? [Other]'].str.lower().astype('category')
-df['In which discipline is your highest academic qualification? [Other]'].categories
-#pd.crosstab(df['In which discipline is your highest academic qualification? [Other]'], columns='Disciplines -- Other')
+df = pd.read_csv('./dataset/2017 Cdn Research Software Developer Survey - Public data.csv')
+# df['In which discipline is your highest academic qualification? [Other]'] = df['In which discipline is your highest academic qualification? [Other]'].str.lower().astype('category')
 
+
+# # Recode the different categories to an existing (High level NSERC code).
+def recode_disciplines(x):
+    """
+    Recode values from Disciplines -- Others to match
+    prexisting categories from NSERC code
+    """
+    dict_of_replacement = {'bioinfo': 'Bioinformatics',
+                           'computer': 'Information technology',
+                           'informatique': 'Information technology',
+                           'history': 'Social sciences and humanities',
+                           'biophysics': 'Physics',
+                           'software': 'Information and communication services',
+                           'dance': 'Social Sciences and Humanities',
+                           'musique': 'Social Sciences and Humanities'}
+    if not pd.isnull(x):
+        for k in dict_of_replacement:
+            if k.lower() in str(x).lower():
+                return dict_of_replacement[k].capitalize()
+
+        return 'Other'
+
+# # Apply the re encoding to the column discipline Other
+df['In which discipline is your highest academic qualification? [Other]'] = df['In which discipline is your highest academic qualification? [Other]'].apply(recode_disciplines)
+
+# Now it just need to merge the two discipline column into one
 
 # In[15]:
 
-dict_of_replacement = {'bioinfo': 'bioinformatics', 
-                       'computer':'information technology', 
-                       'informatique': 'information technology',
-                       'music'
-df['In which discipline is your highest academic qualification? [Other]'].replace('bioinfo', value='bioinformatics', regex=True)
+df['In which discipline is your highest academic qualification?'].replace('Other',df['In which discipline is your highest academic qualification? [Other]'], inplace=True)
+
+df['In which discipline is your highest academic qualification?'] = df['In which discipline is your highest academic qualification?'].apply(str.capitalize).astype('category')
+
 
 
 # In[82]:
@@ -152,10 +188,7 @@ pd.crosstab(df['In which discipline is your highest academic qualification?'], c
 
 # In[83]:
 
-df['In which discipline is your highest academic qualification?'].value_counts().plot(kind='bar', sort_columns=True)
+df['In which discipline is your highest academic qualification?'].value_counts().plot(kind='barh', sort_columns=True)
 
 
-# In[ ]:
-
-
-
+# In[84]:
