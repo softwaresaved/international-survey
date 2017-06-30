11d18e64a07b865979975cf163034436d282e1e1
Olivier PHILIPPE <olivier.philippe@gmail.com>
2017-06-30 14:52:22 +0100

    Transform into class, added the structured cleaning and output the right file into to_json

--- analysis/data_process/uk_2017/cleaning.py
+++ analysis/data_process/uk_2017/cleaning.py
@@ -14,7 +14,12 @@ from config import CleaningConfig
 
 
 class CleaningData(CleaningConfig):
-
+    """
+    Class to perfom some generic cleaning and recording an anonimized
+    dataframe into a csv file at the end. Also record a json file
+    that take all the questions headers from limesurvey data and match them
+    with the question file to enable automatic process during the plotting
+    """
     def __init__(self, df):
         """
         """
@@ -32,8 +37,15 @@ class CleaningData(CleaningConfig):
         self.df = self.duplicating_other(self.df)
         if self.structured:
             self.survey_structure = self.get_survey_structure()
+            self.grouping_question(self.df)
         else:
             pass
+
+    def compare_question(self):
+        """
+        Compare the question from the question file
+        """
+        pass
 
     def get_survey_structure(self):
         """
@@ -45,11 +57,12 @@ class CleaningData(CleaningConfig):
             for row in reader:
                 section = row[0]
                 code = row[1]
+                # question = self.cleaning_some_white_space(row[2])
                 question = row[2]
                 type_question = row[4]
                 file_answer = '{}/{}.csv'.format(self.answer_folder, row[4])
                 result_dict[code] = {'section': section,
-                                     'question': question,
+                                     'original_question': question,
                                      'type_question': type_question,
                                      'file_answer': file_answer}
         return result_dict
@@ -92,6 +105,24 @@ class CleaningData(CleaningConfig):
         df = df.loc[:, ~df.columns.str.contains('Group time')]
         return df
 
+    def cleaning_some_white_space(self, string):
+        """
+        Clean some white space issues encountered in some text
+        questions
+        """
+        print(string)
+        # Some columns have a unbreakable space in their name, replace it
+        string = string.replace('\xa0', ' ')
+        string = string.replace('\u00a0', ' ')
+        # Some columns have a tabular instead of a space
+        string = string.replace('\t', ' ')
+        # FIXME compile the regex into the __init__()
+        string = re.sub('(?<=\s) +|^ +(?=\s)| (?= +[\n\0])', ' ', string)
+        # Replace all ending white space
+        string = string.strip()
+        print(string)
+        return string
+
     def cleaning_columns_white_space(self, df):
         """
         Various cleaning white spaces in columns name
@@ -104,14 +135,10 @@ class CleaningData(CleaningConfig):
         :return:
             df dataframe(): the same df but with cleaned columns
         """
-        # Some columns have a unbreakable space in their name, replace it
-        df.columns = df.columns.str.replace('\xa0', ' ')
-        # Some columns have a tabular instead of a space
-        df.columns = df.columns.str.replace('\t', ' ')
-        # FIXME compile the regex into the __init__()
-        df = df.rename(columns=lambda x: re.sub('(?<=\s) +|^ +(?=\s)| (?= +[\n\0])', ' ', x))
-        # Replace all ending white space
-        df.columns = df.columns.str.strip()
+        # df.columns = df.columns.str.replace('\xa0', ' ')
+        # df.columns = df.columns.str.replace('\t', ' ')
+        df = df.rename(columns=lambda x: self.cleaning_some_white_space(x))
+        # df.columns = df.columns.str.strip()
         return df
 
     def cleaning_missing_na(self, df):
@@ -127,74 +154,8 @@ class CleaningData(CleaningConfig):
         df.replace("Don't want to answer", np.NaN, inplace=True)
         return df
 
-    def duplicating_other(self, df):
-        """
-        When there is an option for 'Other', the column contains the value typed
-        by the participants. However, to plot later, it is better to recode all this
-        values as 'Yes', as for the other items. Then duplicating these value in another
-        column with the tags [Other Raw] for later analysis if we want to analyse it in
-        details.
-        Creating the tag [Other Raw] at the beginning of the column name to avoid that
-        columns being picked up by the grouping_question()
-
-        :params:
-
-        :return:
-            :df dataframe(): Return the modified dataframe
-        """
-        for col in df.columns:
-            if col[-7:] == '[Other]':
-                # Duplicate the column
-                df['[OTHER_RAW] '+ col] = df[col]
-                # Replace all the values with 'Yes'
-                df[col] = df[col].apply(lambda x: 'Yes' if not pd.isnull(x) else np.nan)
-        return df
-
     def grouping_question(self, df):
-        """
-        Group question together by merging them when they have a [TAG]
-        at the end of their column name.
-        They group them in a list of list to be able to parse later.
-        The list as the columns name for later operation on the df.
-        1. Loop through the columns of the dataframe
-        2. Check if the question is similar to the previous one,
-        if it is True, it add it to a list until it is False
-        3. When it is False, add that list to a larger list that
-        contains all the columns split in group lists.
 
-        :params:
-            pd.dataframe(): dataframe to parse all columns
-
-        :return:
-            list(): a list() of list() of columns name str(). Each list
-            contains one group of question.
-            If a list only contains one question, this question doesn't belong
-            to any group
-        """
-        def compare_question(current_question, previous_question, current_particule, previous_particule):
-            """
-            """
-            current_q = current_question.replace(current_particule, '')
-            previous_q = previous_question.replace(previous_particule, '')
-            if current_q == previous_q:
-                return True
-
-        def get_particule(col):
-            """
-            Do a regex match to get the bracket content and return the
-            matched string, or None if not
-
-            :param:
-                col str(): the column name to apply the regex on it
-
-            :return:
-                last_bit str(): the str between the bracket (w/ the bracket)
-                None, if no match is found
-            """
-            re_match_brac = '\[([^]]+)\]'
-            last_bit = re.search(re_match_brac, col)
-            if last_bit:
-                return last_bit[0]  # If [0], output w/ [], if [1] output w/o []
 
         def check_similar_q(col, full_list, current_list):
             """
@@ -214,15 +175,14 @@ class CleaningData(CleaningConfig):
                 if the current question was different than the previous one
                 current_list list(): the same current_list, appended with the current
                 question if similar to the last element of it or a new one only composed
-                of the current question if it was different
+                of the current question if it was different.
             """
-            # if len(current_list) > 0:
-            current_particule = get_particule(col)
-            previous_particule = get_particule(current_list[-1])
-            if current_particule and previous_particule:
-                if compare_question(col, current_list[-1], current_particule, previous_particule):
-                    current_list.append(col)
-                    return full_list, current_list
+            current_code = get_question_code(col)
+            previous_code = get_question_code(current_list[-1])
+            if current_code == previous_code:
+                current_list.append(col)
+                return full_list, current_list
+                # if compare_question(col, current_list[-1], current_code, previous_code):
             full_list.append(current_list)
             current_list = [col]
             return full_list, current_list
@@ -248,111 +208,93 @@ class CleaningData(CleaningConfig):
                     i+=1
             return single_q, group_q
 
-        grouped_question = list()
-        for col in df.columns:
-            try:
-                grouped_question, current_list = check_similar_q(col, grouped_question, current_list)
-            except (NameError, TypeError):  # NameError when it parsed the 1st column
-                current_list = [col]
-
-        single_q, group_q = split_group(grouped_question)
-        return single_q, group_q
-
-    def check_answers(df, questions, answer_item_dict):
-
-        def get_unique_answer(df, questions):
+        def get_question_code(column_name, element_to_return):
             """
-            Create a set of unique answers for
-            each of grouped questions
+            :params:
+                :column_name str(): text of the column name to split
+                and extract the text according on the name format
+                output by limesurvey
+                :element_to_return int(): Which element of the code to output
+                if 0, it output the expected code `edu1`, `socio2`, ...
+                However in case of multiple questions presented in one grid in limesurvey
+                the code is stored on the second element of the format (ie `likerttime1[perfCheck1]`)
+                in that case, if it is 1, it return the second element
             """
-            for group in questions:
-                unique_answer = set()
-                for q in group:
-                    unique_answer = set(unique_answer | set(df[q].unique()))
-                    # unique_answer.add(df[q].unique())
-                # Remove the nan element
+            # Follow the structure given by
+            # limesurvey
+            splitted_col = column_name.split('.')
+            if element_to_return == 0:
+                code = splitted_col[0].split('[')[0]
+            if element_to_return == 1:
                 try:
-                    unique_answer.remove(np.nan)
+                    code = splitted_col[0].split('[')[1][:-1]  # -1 to remove the last ] in the string
+                except IndexError:  # In case not splitting like that just return the other code
+                    return  splitted_col[0].split('[')[0]
+            return code
+
+        if self.structured:
+            for col in df.columns:
+                code = get_question_code(col, 0)
+                try:
+                    self.survey_structure[code].setdefault('survey_q', []).append(col)
                 except KeyError:
-                    pass
-                yield group, unique_answer
-
-        def common_element(dict_of_answers, set2, *args):
-            """
-            If the len of common element between the two sets
-            are at least 0.5 of the len of the set1
-            return True, False otherwise
-            """
-            def check_set(set1, set2, *args):
-                element_to_remove = ['other', 'none', 'prefer not to say', 'other/not listed']
-                set11 = set([str(x).lower() for x in set1 if str(x).lower not in element_to_remove])
-                set22 = set([str(x).lower() for x in set2 if str(x).lower not in element_to_remove])
-
-                common_set = set11.intersection(set22)
-                if len(common_set) >= (len(set22) -1):  # In case Nan or other none response
-                    # print(args[0])
-                    # print(common_set)
-                    return True
-
-            for q in dict_of_answers:
-                if check_set(dict_of_answers[q], set2, *args):
-                    # print(q)
-                    return q
-
-        def check_numbers(input_set):
-            """
-            Check if the strings can be converted in int
-            In that case, it means that it is either a discrete
-            scale or a likert scale
-            """
-            def f(x):
+                    code = get_question_code(col, 1)
+                    try:
+                        self.survey_structure[code].setdefault('survey_q', []).append(col)
+                    except KeyError:
+                        pass
+                        # if code == 'OTHER_RAW':
+                        #     self.survey_structure['OTHER_RAW'] = dict()
+                        #     self.survey_structure['OTHER_RAW'].setdefault('survey_q', [].append(col))
+                        # elif code == 'SQ001' or code == 'SQ002':
+                        #     self.survey_structure['satisGen'] = dict()
+                        #     self.survey_structure['satisGen'].setdefault('survey_q', [].append(col))
+                        # else:
+                        #     print(code)
+                        #     print(col)
+        else:
+            grouped_question = list()
+            for col in df.columns:
                 try:
-                    return float(x)
-                except ValueError:
-                    None
+                    grouped_question, current_list = check_similar_q(col, grouped_question, current_list)
+                except (NameError, TypeError):  # NameError when it parsed the 1st column
+                    current_list = [col]
 
-            set_int = set([x for x in input_set if f(x)])
-            if len(set_int) == len(input_set):
-                return 'discrete'
-            # Assuming that a likert scale did not have the answer 0 - Disagree or 10 - Disagree, which means it only have
-            # int(). However, as it can only have 8 items (maximum 10 items with 2 items that cannot be converted
-            # to int(), it should satisfied this condition to be considered as likert_undefined
-            if len(set_int) < 8 and len(set_int) >=3:
-                if len(input_set) <=10:  # Lowest possible likert scale exluding the extremity
-                    return 'likert_undefined'
+            self.single_q, self.group_q = split_group(grouped_question)
 
-        def get_type_data(answer_item_dict, unique_answer, *args):
-            """
-            """
-            if len(unique_answer) <= 1:
-                return 'single_item'
+    def duplicating_other(self, df):
+        """
+        When there is an option for 'Other', the column contains the value typed
+        by the participants. However, to plot later, it is better to recode all this
+        values as for the other items, then duplicating these values in another
+        column with the tags [Other Raw] to keep the information for later.
+        There are two cases when [OTHER] columns have been created.
+            1. In case of a multiple choice question (several answer can be selected)
+                - The value needs to be recoded into 'Yes' and the column kept
+            2. In case of a drop down type of question (only one answer can be selected)
+                - The column can be just renamed into [Other Raw] for later analysis, the
+                value 'Other' being already encoded in the principal column question
+        Creating the tag [Other Raw] at the beginning of the column name to avoid that
+        columns being picked up by the grouping_question()
 
-            number = check_numbers(unique_answer)
-            if number == 'discrete':
-                return number
-
-            q_from_csv = common_element(answer_item_dict, unique_answer, *args)
-            if q_from_csv:
-                return q_from_csv
-
-            else:
-                return 'messy_data'
-
-        type_question = dict()
-        # Need to loop through the list because some questions are grouped,
-        # and the single questions are single element in a list
-        for group, unique_answer in get_unique_answer(df, questions):
-            type_answer = get_type_data(answer_item_dict, unique_answer, group[0])
-            type_question.setdefault(type_answer, []).append(group)
-
-        return type_question
+        :params:
+            :df dataframe(): Dataframe containing the data
+        :return:
+            :df dataframe(): Return the modified dataframe
+        """
+        for col in df.columns:
+            if col[-7:] == '[Other]':
+                # Duplicate the column
+                df['[OTHER_RAW]. '+ col] = df[col]
+                # Replace all the values with 'Yes'
+                df[col] = df[col].apply(lambda x: 'Yes' if not pd.isnull(x) else np.nan)
+        return df
 
     def write_config_file(self):
         """
         """
         if self.structured:
             dict_to_write = self.survey_structure
-            pass
         else:
             dict_to_write = {'single_questions': self.single_q,
                              'grouped_questions': self.group_q}
@@ -372,44 +314,31 @@ def main():
     # from include import plotting
     # When using Ipython within vim
     matplotlib.use('TkAgg')
-
-    # When using within jupyter
-    # get_ipython().magic('matplotlib inline')  # Activat that line to use in Jupyter
-
     import matplotlib.pyplot as plt
+
     #  When using this script with ipython and vim
     plt.ion()
     plt.show()
     pd.set_option('display.max_rows', 300)
+
     # Load dataset
     df = pd.read_csv(CleaningConfig.raw_data)
-    list(df.columns)
 
-    cleaning_process = CleaningData(df)
-    cleaning_process.cleaning()
-    cleaning_process.write_df()
-    cleaning_process.write_config_file()
-
-    #
+    # # SPECIFIC UK
+    # # Overall, as soon as the participants passed the first page, they reached the last page.
+    # # In consequence, if a participant passed the first page, (s)he is kept.
     # # # The last page is the last page the participants reached. To
     # # # do a compromise between keeping some and getting rid of the participants that haven't complete
     # # # enough answers
     # nb_answer = pd.DataFrame(df['Last page'].value_counts()).sort_index(ascending=True)
     # nb_answer['cumfreq'] = nb_answer.cumsum()
     # nb_answer.plot(kind='bar')
-    #
-    # # SPECIFIC UK
-    # # Overall, as soon as the participants passed the first page, they reached the last page.
-    # # In consequence, if a participant passed the first page, (s)he is kept.
     # df = df.loc[df['Last page']> 1]
-    #
-    # # # Replace Yes and No to Boolean when it is possible
-    # df = dropping_lime_useless(df)
-    # df = cleaning_columns_white_space(df)
-    # df = cleaning_missing_na(df)
-    # df = duplicating_other(df)
-    # single_q, group_q = grouping_question(df)
-    #
+
+    cleaning_process = CleaningData(df)
+    cleaning_process.cleaning()
+    cleaning_process.write_df()
+    cleaning_process.write_config_file()
 
 
 if __name__ == "__main__":
