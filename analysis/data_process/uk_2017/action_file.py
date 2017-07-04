0cc0716d7ab338288e5da45ad850216af519a6ce
Olivier PHILIPPE <olivier.philippe@gmail.com>
2017-07-04 22:16:52 +0100

    Add functional function to group question by type

--- analysis/data_process/uk_2017/action_file.py
+++ analysis/data_process/uk_2017/action_file.py
@@ -20,6 +20,26 @@ in the same folder
 """
 
 
+def group_and_split_q(group_question):
+    """
+    """
+    regroup_q, regroup_txt_q = list(), list()
+    previous_type = None
+    for q in group_question:
+        current_type = group_question[q]['answer_format'].lower()
+        if previous_type is not None:
+            if current_type != previous_type:
+                yield regroup_q, regroup_txt_q, previous_type
+                regroup_q, regroup_txt_q = list(), list()
+
+        survey_q = group_question[q]['survey_q']
+        original_q = group_question[q]['original_question']
+        regroup_q.extend(survey_q)
+        regroup_txt_q.append(original_q)
+        previous_type = current_type
+
+    yield regroup_q, regroup_txt_q, previous_type
+
 
 def main():
     pd.set_option('display.max_rows', 300)
@@ -41,18 +61,25 @@ def main():
     for s in cleaning_process.structure_by_section:
         section = cleaning_process.structure_by_section[s]
         notebook.add_section(s)
-        for question in section:
-            original_question = section[question]['original_question']
-            try:
-                list_question = section[question]['survey_q']
-                answer_format = section[question]['answer_format']
-                file_answer = section[question]['file_answer']
-                counted_value = get_count(list_question, answer_format)
-                notebook.add_question_title(original_question)
-                notebook.add_freq_table(counted_value, answer_format)
-                notebook.add_plot(counted_value, answer_format, file_answer)
-            except Exception:  #FIXME Need to record all exception in a separated logfile for further investigation
-                pass
+        for group in section:
+            # In that case, there is only one question in the group and
+            # the list of question should be passed too the counting
+            # and plotting directly
+            # key_group = list(section[group].keys())[0]
+            # original_question = section[group][key_group]['original_question']
+            # notebook.add_question_title(original_question)
+            notebook.add_group(group)
+            for question in group_and_split_q(section[group]):
+                list_questions = question[0]
+                original_questions = question[1]
+                answer_format = question[2]
+                try:
+                    notebook.add_count(list_questions, answer_format)
+                    # notebook.add_freq_table(list_questions, answer_format)
+                    # notebook.add_plot(counted_value, answer_format, file_answer)
+                except KeyError:
+                    print('Error for the question: {}'.format(original_question))
+
     print('Running notebook')
     notebook.run_notebook()
     # Catching error before saving
