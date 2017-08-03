98f699f42c0a2699d6e75d47c46e1ffde56e4933
Olivier PHILIPPE <olivier.philippe@gmail.com>
2017-08-03 15:47:52 +0100

    init the file to automate the process of creating the survey file

---
+++ survey_creation/creating_survey.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+__author__ = 'Olivier Philippe'
+
+
+"""
+Limesurvey offers the possibility to import/export a TSV file containing all the information
+of the survey structure. Using this possibility, this script aims to translate the files used
+by the different groups to creates their survey, into a compatible TSV file.
+It take only the argument of the folder where all the information is stored, and create a new
+compatible file.
+All information about the TSV file structure can be retrieved here:
+    https://manual.limesurvey.org/Tab_Separated_Value_survey_structure
+"""
+
+import csv
+import sys
+import os
+import logging
+
+
+
+
+def main():
+    folder = sys.argv[1]
+    question_file = os.path.join(folder, folder + '.' + 'csv')
+    with open(question_file, 'r') as f:
+        print(f)
+
