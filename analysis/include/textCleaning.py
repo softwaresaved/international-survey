6f194fffc1ba15134272e727ff65a418aaee30f0
Olivier PHILIPPE <olivier.philippe@gmail.com>
2017-08-09 13:28:31 +0100

    Fix the pixelised txt for the wordcloud

--- analysis/include/textCleaning.py
+++ analysis/include/textCleaning.py
@@ -24,7 +24,7 @@ def wordcloud(df, column):
     # TODO make a quicker way than that
     txt = list(itertools.chain.from_iterable(df[column].values))
     cleaned_txt = clean_txt(txt)
-    return WordCloud(background_color='white').generate(cleaned_txt)
+    return WordCloud(background_color='white', width=1500, height=800).generate(cleaned_txt)
 
 
 if __name__ == "__main__":
@@ -43,10 +43,8 @@ if __name__ == "__main__":
     plt.ion()
     df = pd.read_csv('../uk_2017/data/cleaned_data.csv')
 
-    col_with_txt = df['skill2[SQ001]. What skills would you like to acquire or improve to help your work as a Research Software Engineer? The skills can be technical and non-technical. [Skill 1]']
-    txt_to_plot = clean_txt(col_with_txt)
-    print(txt_to_plot)
-    plot_wordcloud = wordcloud(txt_to_plot)
+    column = 'skill2[SQ001]. What skills would you like to acquire or improve to help your work as a Research Software Engineer? The skills can be technical and non-technical. [Skill 1]'
+    plot_wordcloud = wordcloud(df, column)
     plt.imshow(plot_wordcloud, cmap=plt.cm.gray, interpolation="bilinear")
 
     plt.axis('off')
