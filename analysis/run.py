b04c87330604d1bd674a47a6a227eac3785c3f86
Olivier PHILIPPE <olivier.philippe@gmail.com>
2017-08-09 15:39:18 +0100

    Added the ranking to the visualisation

--- analysis/run.py
+++ analysis/run.py
@@ -66,6 +66,7 @@ def main():
 
                 if answer_format == 'ranking':
                     notebook.add_count(list_questions, answer_format, file_answer)
+                    notebook.add_percentage()
                     notebook.add_plot(answer_format)
 
     print('Running notebook')
