d5d05c45ca4610d9d24671775f874e4b05726f65
Olivier PHILIPPE <olivier.philippe@gmail.com>
2017-09-04 12:48:01 +0200

    Creating the function to read the infile and to create a dict with the appropriate headers

--- survey_creation/creating_survey.py
+++ survey_creation/creating_survey.py
@@ -18,7 +18,9 @@ All information about the TSV file structure can be retrieved here:
 import csv
 import sys
 import os
+from collections import OrderedDict
 from include.logger import logger
+from config import creationConfig
 
 RUNNING = 'dev'
 
@@ -27,19 +29,46 @@ if RUNNING == 'dev':
 elif RUNNING == 'prod':
     DEBUGGING='INFO'
 
-
 logger = logger(name='creating survey', stream_level=DEBUGGING)
 
 
+def creating_dictionary():
+    """
+    Create the dictionary to be used to translate csv file to tsv
+    """
+    return OrderedDict((k, None) for k in creationConfig.main_headers)
+# [OrderedDict((k, d[k](v)) for (k, v) in l.iteritems()) for l in L]
 
 
-def main():
-    folder = sys.argv[1]
-    question_file = os.path.join(folder, folder + '.' + 'csv')
+def read_survey_file(folder):
+    """
+    Read the survey csv file and yield each line as a dictionary
+    """
+    question_file = os.path.join(folder, '.'.join([folder, 'csv']))
     with open(question_file, 'r') as f:
         csv_f = csv.reader(f, delimiter=';', quotechar='"')
         for row in csv_f:
-            print(row)
+            yield row
+
+
+def write_row_in_file(input_file, row):
+    """
+    """
+    with open(input_file, 'a') as f:
+        # f.write({k: v for
+        for key in creationConfig.main_headers:
+            pass
+
+
+def main():
+    output_survey = creating_dictionary()
+    for k in output_survey:
+        print(k, output_survey[k])
+    # print(output_survey)
+    folder = sys.argv[1]
+    for line in read_survey_file(folder):
+        pass
+        # print(line)
 
 
 if __name__ == "__main__":
