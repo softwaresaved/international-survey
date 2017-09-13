4437664a8101ba44d6ac8fc61ecd5a4f91b046cf
Olivier PHILIPPE <olivier.philippe@gmail.com>
2017-09-13 11:59:26 +0200

    Remove the Other option from the listAnswer, add it into the main_csv and script takes it into account

--- survey_creation/config/de_17.py
+++ survey_creation/config/de_17.py
@@ -8,13 +8,15 @@ class config:
     # To modify, just add the keys of the dictionary
     header_to_modify = [{'class': 'S', 'name': 'sid', 'text': '421498'},
                         {'class': 'S', 'name': 'admin_email', 'text': 'olivier.philippe@soton.ac.uk'}]
-    # Same as header_to_modify
-    description_to_modify = []
-    languages_to_add = 'de'
-
     # Add header and description. Tuple of a dictionary + the position where it is supposed
     # to be inserted
     header_to_add = [({'class': 'S', 'name': 'additional_languages', 'text': 'de'}, 12)]
+
+    # Same as header_to_modify
+    description_to_modify = []
+    description_to_add = []
+    languages_to_add = 'de'
+
     # The index positions starts at 0
     # Adding the survey title to the global description. The index position is at 0 and the structure of the dictionary is as follow:
     # {'class': 'SL', 'name': 'surveyls_title', 'text': 'DEMO -- RSE Survey -- 2017 -- EN', 'language': 'en'}
