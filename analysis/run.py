5596f1763edba33b6cdbef3d964fb81a143df24b
Olivier PHILIPPE <olivier.philippe@gmail.com>
2017-10-06 16:50:49 +0200

    add argument parser

--- analysis/run.py
+++ analysis/run.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+import sys
+import getopt
 import pandas as pd
 from include.config import CleaningConfig, CountingConfig, NotebookConfig
 from include.preprocessing import CleaningData
@@ -18,8 +19,41 @@ in the same folder
 """
 
 
+def get_arguments(argv):
+    """
+    """
+    country = None
+    year = None
+    try:
+        opts, args = getopt.getopt(argv, 'hc:y:', ['country=', 'year='])
+    except getopt.GetoptError:
+        print('run.py -c <country> -y <year>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('run.py -c <country> -y <year>')
+            sys.exit()
+        elif opt in ('-c', '--country'):
+            country = arg
+        elif opt in ('-y', '--year'):
+            year = arg
+    if country and year:
+        return country, year
+    else:
+        print('Need a country and a year. Please use the following command:\n' +
+              '\trun.py -c <country> -y <year>')
+        sys.exit(2)
+
 def main():
+
     pd.set_option('display.max_rows', 300)
+
+    # Get which country and which year to create the analysis
+    country, year = get_arguments(sys.argv[1:])
+
+    # Getting the different location files from the config file and
+    folder = '{}_{}'.format(year, country)
+
 
     # Load dataset
     df = pd.read_csv(CleaningConfig.raw_data)
