e15f3d50e67c173ec26b1dd20d964c3c913c34d8
Olivier PHILIPPE <olivier.philippe@gmail.com>
2017-10-17 14:06:36 +0200

    Progressing on the rewrite of config file and implementation in the different script files

--- analysis/cleaning.py
+++ analysis/cleaning.py
@@ -12,40 +12,16 @@ import sys
 import getopt
 import pandas as pd
 from include.preprocessing import CleaningData
-
-
-def get_arguments(argv):
-    """
-    """
-    country = None
-    year = None
-    try:
-        opts, args = getopt.getopt(argv, 'hc:y:', ['country=', 'year='])
-    except getopt.GetoptError:
-        print('run.py -c <country> -y <year>')
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt == '-h':
-            print('run.py -c <country> -y <year>')
-            sys.exit()
-        elif opt in ('-c', '--country'):
-            country = arg
-        elif opt in ('-y', '--year'):
-            year = arg
-    if country and year:
-        folder_path = os.path.join(year, country)
-        return folder_path
-    else:
-        print('Need a country and a year. Please use the following command:\n' +
-              '\trun.py -c <country> -y <year>')
-        sys.exit(2)
+from include.config import CleaningConfig
+from include.get_arguments import get_arguments
 
 
 def main():
-
-    folder = get_arguments(sys.argv[1:])
+    year, country = get_arguments(sys.argv[1:])
     # load dataset
-    df = pd.read_csv(raw_data)
+
+    cleaning_config = CleaningConfig(year, country)
+    df = pd.read_csv(cleaning_config.raw_data)
     cleaning_process = CleaningData(df)
     df = cleaning_process.cleaning()
     cleaning_process.write_df()
