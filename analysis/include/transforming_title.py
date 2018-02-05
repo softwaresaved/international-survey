40d16a1c945c7b27c717700d972630c820d3c1e0
Olivier PHILIPPE <olivier.philippe@gmail.com>
2018-02-05 19:37:02 +0100

    Add transforming_title

---
+++ analysis/include/transforming_title.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+In case of several questions representing items (such as for likert) or questions that needs to be grouped
+(such as the questions with severla y/n) it is impossible to have a proper title. Here it is the corresponding dictionary to match the code of the question with the title
+"""
+
